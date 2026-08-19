aff3fe16589b608b5cf5bf2373e775afac7156a0
Hossain Md. Fahim <hossainmdfahim66@gmail.com>
2026-08-19 22:43:57 +0600

    nvim: add latex plugin vimtex

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -603,6 +603,15 @@ require("lazy").setup({
       'vim-voom/VOoM',
     },
     {
+      "lervag/vimtex",
+      lazy = false,     -- we don't want to lazy load VimTeX
+      -- tag = "v2.15", -- uncomment to pin to a specific release
+      init = function()
+        -- VimTeX configuration goes here, e.g.
+        vim.g.vimtex_view_method = "zathura"
+      end
+    },
+    {
       'isakbm/gitgraph.nvim',
       opts = {
         git_cmd = "git",
