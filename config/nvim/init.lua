c3a1b585f8b194e3a372e74c5887686e8b58ff6e
Hossain Md. Fahim <hossainmdfahim66@gmail.com>
2026-08-28 02:56:14 +0600

    nvim(plugin): introduce new plugin hexinspector

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -611,6 +611,52 @@ require("lazy").setup({
         vim.g.vimtex_view_method = "zathura"
       end
     },
+   --  {
+   --    "RaafatTurki/hex.nvim",
+	  -- config = true
+   --  },
+	{
+	  "Punity122333/hexinspector.nvim",
+	  cmd = { "HexEdit", "HexInspect" },
+	  keys = {
+		{ "<leader>zx", function() require("hexinspector").open() end, desc = "Hex Editor" },
+		{
+		  "<leader>zX",
+		  function()
+			vim.ui.input({ prompt = "File path: ", default = vim.fn.expand("%:p") }, function(input)
+			  if input and input ~= "" then
+				require("hexinspector").open(input)
+			  end
+			end)
+		  end,
+		  desc = "Hex Editor (Pick File)",
+		},
+	  },
+	  opts = {
+		-- All options are optional. Shown below are the defaults.
+		-- bytes_per_line = 24,
+		-- max_undo = 200,
+		-- max_memory_file = 64 * 1024 * 1024,
+		-- colors = {
+		--   bg           = "#1a1b26",
+		--   info_bg      = "#1a1b26",
+		--   border       = "#115e72",
+		--   addr         = "#565f89",
+		--   hex          = "#c0caf5",
+		--   ascii        = "#9ece6a",
+		--   null         = "#3b4261",
+		--   cursor_bg    = "#28344a",
+		--   cursor_line_bg = "#1e2030",
+		--   float        = "#ff9e64",
+		--   int          = "#bb9af7",
+		--   uint         = "#7dcfff",
+		--   title        = "#7aa2f7",
+		--   search       = "#f7768e",
+		--   modified     = "#f7768e",
+		--   selection_bg = "#2d4f67",
+		-- },
+	  },
+	},
     {
       'isakbm/gitgraph.nvim',
       opts = {
@@ -2174,6 +2220,25 @@ vim.api.nvim_create_autocmd("SessionLoadPost", {
 })
 
 
+
+-- HexEdit take filename as input
+-- The file being input contains the path to be passed to HexEdit
+vim.keymap.set("n", "<leader>he", function()
+    vim.ui.input({ prompt = "Path file: " }, function(filename)
+        if not filename or filename == "" then
+            return
+        end
+
+        local path = vim.fn.readfile(filename)[1]
+
+        if not path or path == "" then
+            vim.notify("File is empty or unreadable", vim.log.levels.ERROR)
+            return
+        end
+
+        vim.cmd("HexEdit " .. vim.fn.fnameescape(vim.fn.expand(path)))
+    end)
+end)
 
 
 -- ===========================
